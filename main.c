3de8b7ddf3e22a028f0449b64b4ab1bf816ee41c
clarapaupert <95475054+clarapaupert@users.noreply.github.com>
2021-12-03 11:28:23 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//I-b demande les valeurs des composants du circuit
+void valeurComposant(int numeroCircuit, double*R, double*L, double*C)
+{
+    switch (numeroCircuit)
+    {
+    case(1||2):
+        printf("Renseingnez la valeur de R");
+        scanf
+
+    }
+
+}
+
+
+
+
+
+
+
+
+int main()
+{
+
+    return 0;
+}
